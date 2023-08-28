888aca122fa2fb7058b14814a3e033d1e6a5f44c
Yuyang Wang <franktcsn@gmail.com>
2023-08-28 15:51:31 -0700

    modify analyze_earnings

--- Analysis/analyze_earnings.R
+++ Analysis/analyze_earnings.R
@@ -1,5 +1,20 @@
 library(tidyverse)
+library(dplyr)
+library(readr)
+library(cowplot)
+library(infer)
+library(repr)
+library(digest)
+library(gridExtra)
+library(broom)
 
-youtube_data <- read.csv("https://raw.githubusercontent.com/yuwangy/youtube_earning_analysis/main/Global%20YouTube%20Statistics.csv")
+youtube <- read.csv("https://raw.githubusercontent.com/yuwangy/youtube_earning_analysis/main/Global%20YouTube%20Statistics.csv")
 
-head(youtube_data)
+head(youtube)
+
+youtube_tidy <- youtube |>
+  filter(video.views != 0) |>
+  select(Title, video.views, highest_yearly_earnings)
+head(youtube_tidy)
+
+
