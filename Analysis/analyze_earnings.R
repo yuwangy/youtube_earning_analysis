687ccb1f18f6c289253bfa888d6cf25a735f2e71
Yuyang Wang <franktcsn@gmail.com>
2023-09-02 18:34:20 -0700

    update read.me and analyze_earning R file

--- Analysis/analyze_earnings.R
+++ Analysis/analyze_earnings.R
@@ -14,8 +14,32 @@ head(youtube)
 
 youtube_tidy <- youtube |>
   filter(video.views != 0) |>
-  select(Title, video.views, highest_yearly_earnings)
+  select(Title, video.views, subscribers, highest_yearly_earnings)
+
+# preview data:
 head(youtube_tidy)
 
+# Count number of rows of filtered data: 987
+nrow(youtube_tidy)
+
+# generate plot: subscribers vs highest yearly earnings
+subs_plot <- youtube_tidy |>
+  ggplot() +
+  geom_line(aes(x = subscribers, y = highest_yearly_earnings)) +
+  xlab("number of subscribers") +
+  ylab("amount of earnings") +
+  ggtitle("relationship between channel subscribers and yearly earnings")
+
+subs_plot
+
+# generate plot: video views vs highest yearly earnings
+views_plot <- youtube_tidy |>
+  ggplot() +
+  geom_line(aes(x = video.views, y = highest_yearly_earnings)) +
+  xlab("count of video views") +
+  ylab("amount of earnings") +
+  ggtitle("relationship between channel video views and yearly earnings")
+
+views_plot
 
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # youtube_earning_analysis
 Analyzing the correlation between YouTuber Channel performance and estimated earnings, visualizing geographical trend and how certain categories gain popularity
+
+# Summary
+The dataset I used for my analysis contains count of video views, subscribers, 
+monthly earnings, yearly earnings of various popular mainstream YouTuber channels
+on the YouTube Platform. I try to investigate the biggest factors that influence 
+the earnings of different youtube channels. My hypothesis states that the 
+channels with more subscribers and video views generate more earnings. 
+
+# Method
+hypothesis testing, bootstrap sampling, etc.
